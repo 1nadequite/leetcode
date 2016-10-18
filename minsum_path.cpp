69ddddf3348c76292f5553fbf71f9e08f2f284e0
1nadequite <yaretsaa@gmail.com>
2016-10-18 23:49:22 +0300

    minimum path sum

--- minsum_path.cpp
+++ minsum_path.cpp
@@ -16,8 +16,8 @@ public:
     int n = grid.size();
     if (n == 0) return 0;
     int m = grid[0].size();
-    vector<vector<int>> dp(n + 1, vector<int> (m + 1));
-    for (int i = 0; i <= n; ++i) for (int j = 0; j <= m; ++j) dp[i][j] = 1e9;
+    vector<vector<int>> dp(n + 1, vector<int> (m + 1, 1e9));
+
     dp[1][1] = grid[0][0];
     for (int i = 1; i <= n; ++i) {
       for (int j = 1; j <= m; ++j) {
