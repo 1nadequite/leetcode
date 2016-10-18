ff57b8f5a99872688bd73e8eb253ed789673ac5c
1nadequite <yaretsaa@gmail.com>
2016-10-18 23:23:02 +0300

    unique paths

---
+++ uni_paths.cpp
@@ -0,0 +1,32 @@
+// 62. Unique Paths (medium)
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+
+class Solution {
+public:
+  int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m + 1, vector<int> (n + 1));
+    dp[1][1] = 1;
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]);
+      }
+    }
+    return dp[m][n];
+  }
+};
+
+int main() {
+  Solution a;
+  cout << a.uniquePaths(3, 7) << endl;
+
+  return 0;
+}
