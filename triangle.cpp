e560fc081c23bef6907fb1769d792bfd6ed4ed74
1nadequite <yaretsaa@gmail.com>
2016-11-22 23:17:27 +0300

    triangle

--- triangle.cpp
+++ triangle.cpp
@@ -14,10 +14,12 @@ class Solution {
 public:
   int minimumTotal(vector<vector<int>>& triangle) {
     int n = triangle.size();
+    vector<int> dp(n);
+    for (int i = 0; i < n; ++i) dp[i] = triangle[n - 1][i];
     for (int i = n - 2; i >= 0; --i)
       for (int j = 0; j <= i; ++j)
-        triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
-    return triangle[0][0];
+        dp[j] = triangle[i][j] +  min(dp[j], dp[j + 1]);
+    return dp[0];
   }
 };
 
